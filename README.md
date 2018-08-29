6932f700261608a9200681b005da3d48e047eab1
Edwin Cloud <ecloud412@gmail.com>
2018-08-28 20:17:19 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# Startup Idea Generator
+Simple Startup Idea Generator app for iOS made in Xcode 9.4.1
+
+![alt text](./demo.png "Demo")
--- Startup Idea Generator/ViewController.swift
+++ Startup Idea Generator/ViewController.swift
@@ -10,9 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var companies = ["Make School", "Uber", "Netflix", "Facebook", "Google", "Kickstarter", "Spotify", "Airbnb", "Snapchat", "YouTube", "Dropbox", "Amazon", "Craigslist", "Tinder", "Instagram", "Tesla", "Twitter", "SpaceX"]
+    var ideas = ["Dogs", "Books", "Gamers", "Star Wars", "Bitcoin", "Goats", "Zombies", "Rich People", "Swimmers", "Florida", "Vampires", "Dragons", "Internet of Things", "Mars", "Cryptocurrency", "Mosquito Repellent", "Fidget Spinners", "Sun Screen", "Water Bottles", "Lost Travelers", "Superheroes"]
+    
+    @IBOutlet weak var generateButton: UIButton!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var forLabel: UILabel!
+    @IBOutlet var mainView: UIView!
+    
+    @IBAction func generateButtonTapped(_ sender: UIButton) {
+        generateIdea()
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        guard motion == .motionShake else { return }
+        generateIdea()
+    }
+    
+    func setTheme() {
+        label1.text = ""
+        label2.text = ""
+        forLabel.text = ""
+        generateButton.backgroundColor = UIColor.gray
+        generateButton.layer.cornerRadius = 5
+    }
+    
+    func generateIdea() {
+        label1.text = companies[Int(arc4random_uniform(UInt32(companies.count)))]
+        label2.text = ideas[Int(arc4random_uniform(UInt32(companies.count)))]
+        forLabel.text = "for"
+        
+        // Screen Flash Animation Logic Below
+        let curBGColor = mainView.backgroundColor
+        UIView.animate(withDuration: 0.1, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 0.0, options: UIViewAnimationOptions.curveEaseOut, animations: {
+                self.mainView.layer.opacity = 0.6
+                self.mainView.backgroundColor = UIColor.white
+        }, completion: nil)
+        UIView.animate(withDuration: 0.1, delay: 0.1, usingSpringWithDamping: 1.0, initialSpringVelocity: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: {
+            self.mainView.layer.opacity = 1.0
+            self.mainView.backgroundColor = curBGColor
+        }, completion: nil)
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setTheme()
     }
 
     override func didReceiveMemoryWarning() {
